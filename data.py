b677e643115cd2a179509e524265cd3c40160d72
Gennadiy <renardes@gmail.com>
2022-09-11 21:17:43 +0500

    сигнатуры функций read_ini, save_ini, get_player_name

--- data.py
+++ data.py
@@ -7,3 +7,13 @@
 STATS = {'Player1': {'wins': 2, 'ties': 0, 'fails': 0, 'training': True}}
 
 
+def read_ini() -> bool:
+    """Читает из файлов данные и заполняет структуры данных."""
+
+
+def save_ini():
+    pass
+
+
+def get_player_name():
+    pass

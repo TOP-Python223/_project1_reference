ae4612115b3aef07d93c5b28278ad73f434ab2fd
Gennadiy <renardes@gmail.com>
2022-09-17 20:28:50 +0500

    код функции read_ini()

--- data.py
+++ data.py
@@ -1,14 +1,62 @@
 """Дополнительный модуль: работа с данными и файлами данных."""
 
 # импорт из стандартной библиотеки
+from itertools import zip_longest
+from pathlib import Path
+from pprint import pprint
+from sys import path
+from configparser import ConfigParser
 
 
 # глобальные переменные модуля
-STATS = {'Player1': {'wins': 2, 'ties': 0, 'fails': 0, 'training': True}}
+SCRIPT_DIR = Path(path[0])
+players_path = SCRIPT_DIR / 'players.ini'
+saves_path = SCRIPT_DIR / 'saves.ini'
+
+STATS = {}
+SAVES = {}
 
 
 def read_ini() -> bool:
     """Читает из файлов данные и заполняет структуры данных."""
+    players = ConfigParser()
+    players.read(players_path)
+    for player in players.sections():
+        stats = players[player]
+        STATS[player] = {
+            k: int(v) if v.isdecimal() else stats.getboolean(k)
+            for k, v in stats.items()
+        }
+    saves = ConfigParser()
+    saves.read(saves_path)
+    for players in saves.sections():
+        save = saves[players]
+        all_turns = [
+            part.split(',')
+            for part in save['turns'].split('|')
+        ]
+        turns = []
+        for x, o in zip_longest(*all_turns):
+            try:
+                turns += [int(x), int(o)]
+            except TypeError:
+                turns += [int(x)]
+        list_field = [0]*9
+        for i in range(9):
+            try:
+                list_field[i] = turns.index(i) + 1
+            except ValueError:
+                list_field[i] = 0
+        list_field = [
+            [list_field[j] for j in range(i, i+3)]
+            for i in range(0, 9, 3)
+        ]
+        SAVES[tuple(players.split(':'))] = list_field
+    # первый ли запуск
+    if STATS:
+        return False
+    else:
+        return True
 
 
 def save_ini():
